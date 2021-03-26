02a1984da6a1a412a61db0d850642453d8204aa8
rnuesse <ralph_nuesse@bmc.com>
2021-03-26 15:19:43 +0100

    Test JUnit

--- GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTCOB.cbl
+++ GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTCOB.cbl
@@ -14,7 +14,6 @@
       *                                                                *
       *  RUN JCL     - STORED IN XPEDITER/TSO SAMPLIB (CWXTJCLC)       *
       *                                                                *
-      *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
