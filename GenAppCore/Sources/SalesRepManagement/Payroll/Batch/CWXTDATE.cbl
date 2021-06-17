fb167d0714bae1bcca3220ccc8a24df0afca01d8
stupot1966 <stuart_ashby@bmc.com>
2021-06-17 20:22:56 +0100

    Validate the 20.07.01 CLI

--- GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTDATE.cbl
+++ GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTDATE.cbl
@@ -8,7 +8,7 @@
       *  FOR XPEDITER/TSO TRAINING SESSIONS                            *
       *                                                                *
       ******************************************************************
-      * 20.07.01 
+      * 20.07.01 CLI test
        ENVIRONMENT DIVISION.
        DATA DIVISION.
        WORKING-STORAGE SECTION.
