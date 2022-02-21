317adca249634d7aff41d3c8e261f937e07864b1
stupot1966 <stuart_ashby@bmc.com>
2022-02-21 10:21:06 +0000

    Proving

--- GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTCOB.cbl
+++ GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWXTCOB.cbl
@@ -16,7 +16,6 @@
       *                                                                *
       * RNU 2021.08.09-14:58                                           *
       ******************************************************************
-      * mBank
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
