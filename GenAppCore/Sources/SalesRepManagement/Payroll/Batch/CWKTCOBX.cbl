dc2fa2e1804ac9bf67ec61bd3bff0673f043b938
vbugale <103621216+vbugale@users.noreply.github.com>
2022-07-06 17:59:13 +0530

    Update CWKTCOBX.cbl

--- GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWKTCOBX.cbl
+++ GenAppCore/Sources/SalesRepManagement/Payroll/Batch/CWKTCOBX.cbl
@@ -2,7 +2,7 @@
        PROGRAM-ID.       CWKTCOBX.
       ******************************************************************
       *                                                                *
-      ******      C O M P U W A R E   C O R P O R A T I O N   BMC 5:56pm  ******
+      ******      C O M P U W A R E   C O R P O R A T I O N   BMC 5:59pm  ******
       *                                                                *
       *  THIS PROGRAM IS A QSAM COBOL DEMO PROGRAM USED FOR            *
       *  TOPAZ FOR TOTAL TEST TRAINING SESSIONS.                       *
